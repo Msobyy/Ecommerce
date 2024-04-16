7b76385143bbea68ac533641b970ee484cfbf465
Msobyy <sobanwaqar62@gmail.com>
2024-04-17 00:54:08 +0500

    second commit
    Auth middlewares, login api,admin routes authorization

--- Mern/controllers/authController.js
+++ Mern/controllers/authController.js
@@ -1,63 +1,122 @@
 import userModel from "../models/userModel.js";
-import { hashPassword } from "../utils/authHelper.js";
+import { comparePassword, hashPassword } from "../utils/authHelper.js";
+import JWT from 'jsonwebtoken'
 
 
-const registerController=async(req,res)=>{
-    const{name,email,password,phone,address,role}=req.body;
+//Registration Controller
+const registerController = async (req, res) => {
+    const { name, email, password, phone, address, role } = req.body;
 
-   
+
     try {
-         //validations
+        //validations
 
-    if(!name){
-        return res.send({error:"Name is required"});
-    }
-    if(!email){
-        return res.send({error:"Email is required"});
-    }
-    if(!password){
-        return res.send({error:"Password is required"});
-    }
-    if(!phone){
-        return res.send({error:"Phone no is required"});
-    }
-    if(!address){
-        return res.send({error:"address is required"});
-    }
-    if(!role){
-        return res.send({error:"role is required"});
-    }
+        if (!name) {
+            return res.send({ error: "Name is required" });
+        }
+        if (!email) {
+            return res.send({ error: "Email is required" });
+        }
+        if (!password) {
+            return res.send({ error: "Password is required" });
+        }
+        if (!phone) {
+            return res.send({ error: "Phone no is required" });
+        }
+        if (!address) {
+            return res.send({ error: "address is required" });
+        }
+        if (!role) {
+            return res.send({ error: "role is required" });
+        }
 
-    //existing User
+        //existing User
 
-    const existingUser=await userModel.findOne({email});
-    if(existingUser){
-        return res.status(200).send({
-            success:true,
-            message:"Already registered! Please login"
-        })
-    }
+        const existingUser = await userModel.findOne({ email });
+        if (existingUser) {
+            return res.status(200).send({
+                success: true,
+                message: "Already registered! Please login"
+            })
+        }
 
-    const hashedPassword=await hashPassword(password);
-    const newUser= await userModel.create({name,email,password:hashedPassword,address,phone,role});
-    res.status(201).send({
-        success:true,
-        message:"User Registered Successfully",
-        user:newUser
-    });
+        const hashedPassword = await hashPassword(password);
+        const newUser = await userModel.create({ name, email, password: hashedPassword, address, phone, role });
+        res.status(201).send({
+            success: true,
+            message: "User Registered Successfully",
+            user: newUser
+        });
 
     } catch (error) {
         console.log(error);
         res.status(500).send({
-            success:false,
-            message:'Error in Registration',
+            success: false,
+            message: 'Error in Registration',
             error
         })
     }
-} 
+}
+const loginController = async (req, res) => {
 
 
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(404).send({
+                success: false,
+                message: 'Invalid Email or Password'
+            });
+        }
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: 'User does not exists'
+            });
+        }
+        const matched = await comparePassword(password, user.password);
+        if (!matched) {
+            return res.status(404).send({
+                success: false,
+                message: 'Invalid Password'
+            });
+        }
+        //JWT token
+        const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        res.status(200).send({
+            success: true,
+            message: 'User Login Successfully',
+            user: {
+                name: user.name,
+                email: user.email
+            },
+            token
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error in Login',
+            error
+
+        })
+    }
+}
+
+//test
+const testController=(req,res)=>{
+    try {
+        res.send("Protected Route");
+    } catch (error) {
+        
+    }
+}
 
 export {
-    registerController
+    registerController,
+    loginController,
+    testController
 }
--- Mern/routes/authRoute.js
+++ Mern/routes/authRoute.js
@@ -1,11 +1,19 @@
 import express  from "express";
-import {registerController} from "../controllers/authController.js"
+import {registerController,loginController,testController} from "../controllers/authController.js"
+import { isAdmin, requireAuth } from "../middlewares/authMiddleware.js";
+
 
 //router object
 const router=express.Router();
 
 //routes
+
+//REGISTRATION
 router.post('/register',registerController);
+//LOGIN
+router.post('/login',loginController);
+//test
+router.get('/test',requireAuth,isAdmin,testController);
 
 
 export default router   
